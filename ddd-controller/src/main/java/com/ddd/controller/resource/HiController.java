2d1d3f2831868b7fdbc08ae8e946f7a15f2d9eaf
TuanHung1995 <tuanhungg1995@gmail.com>
2025-03-23 13:11:57 +0700

    ratelimiter, circuitbreaker and actuator

--- ddd-controller/src/main/java/com/ddd/controller/resource/HiController.java
+++ ddd-controller/src/main/java/com/ddd/controller/resource/HiController.java
@@ -1,10 +1,15 @@
 package com.ddd.controller.resource;
 
 import com.ddd.ddd.application.service.event.EventAppService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.security.SecureRandom;
 
 @RestController
 @RequestMapping("/hi")
@@ -13,9 +18,39 @@ public class HiController {
     @Autowired
     private EventAppService eventAppService;
 
+//    @Autowired
+    private RestTemplate restTemplate = new RestTemplate();
+
     @GetMapping("/hello")
+    @RateLimiter(name = "backendB", fallbackMethod = "fallbackHello")
     public String hello() {
         return eventAppService.sayHi("world");
+    }
+
+    @GetMapping("/hi")
+    @RateLimiter(name = "backendA", fallbackMethod = "fallbackHello")
+    public String hi() {
+        return eventAppService.sayHi("Hi");
+    }
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    @GetMapping("/hi/v1")
+    @CircuitBreaker(name = "checkRandom", fallbackMethod = "fallbackCircuitBreaker")
+    public String circuitBreaker() {
+        int productId = secureRandom.nextInt(20) + 1;
+
+        String url = "https://fakestoreapi.com/products/" + productId;
+
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    public String fallbackHello(Throwable t) {
+        return "Too many requests, please try again later.";
+    }
+
+    public String fallbackCircuitBreaker(Throwable t) {
+        return t.getMessage();
     }
 
 }
